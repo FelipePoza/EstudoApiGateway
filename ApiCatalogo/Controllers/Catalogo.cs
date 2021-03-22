09a5d2276a81912dfc10a4a3a0fc843e816038d8
Felipe Poza <Felipe_E_P@hotmail.com>
2021-03-22 19:07:44 -0300

    Criação de API gateway para estudo e testes

---
+++ ApiCatalogo/Controllers/Catalogo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class Catalogo : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string[]> RetornarProduto()
+        {
+            string[] lista = new string[3];
+            lista[0] = "Chocolate";
+            lista[1] = "HD Externo";
+            lista[2] = "Mouse";
+            return Ok(lista);
+        }
+    }
+}
---
+++ ApiPedido/Controllers/Pedido.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPedido.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class Pedido : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string[]> ListarPedido()
+        {
+            string[] lista = new string[2];
+            lista[0] = "pedido 1";
+            lista[1] = "pedido 2";
+            return Ok(lista);
+        }
+    }
+}
